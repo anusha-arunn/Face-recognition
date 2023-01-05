f7e871c4665d0594be389fc10aa1f6d38be6b795
anusha-arunn <anusha.arunn@gmail.com>
2023-01-05 16:15:35 +0530

    Face recognition

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+    "dlib", "opencv-python", "numpy", "imutils", "tqdm"
+]
+
+test_requirements = [
+]
